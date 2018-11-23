e205838e2447b32498d7da5152f0f36be1f1daa3
aluno senai <pedro.gui.sena@gmail.com>
2018-11-23 21:41:26 -0200

    Segundo Commit

--- src/repositorioaula/RepositorioAula.java
+++ src/repositorioaula/RepositorioAula.java
@@ -15,7 +15,10 @@ public class RepositorioAula {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Mundo Senai"
+                + ""
+                + ""
+                + "!");
     }
     
 }
